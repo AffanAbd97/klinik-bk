253fa8ba239a48de0fc27a612a431de0da887429
Affan-97 <affana959@gmail.com>
2024-01-07 14:45:08 +0700

    fix:push unsaved file

--- resources/views/pages/home-dokter/periksa/create.blade.php
+++ resources/views/pages/home-dokter/periksa/create.blade.php
@@ -95,7 +95,7 @@
                     <div>
                         <label for="biaya" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Biaya
                             Periksa</label>
-                        <input type="number" name="biaya" id="biaya" value="100000" readonly disabled
+                        <input type="number" name="biaya" id="biaya" value="100000" readonly 
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             placeholder="10000">
                         @error('biaya')
